af64e77226412339fd7acba451adf51bd81f02a0
Holly Tancredi <tancredi.holly@gmail.com>
2016-01-06 18:14:07 -0500

    cleanup

--- bshell/bshell.c
+++ bshell/bshell.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
 
 void commands() {
     char *line = NULL;
-    char **args;
+    char **args = NULL;
     char *status = NULL;
     char **iterator = NULL;
     size_t len = 0;
@@ -47,7 +47,6 @@ char **parse_args(char *line) {
     while (token != NULL) {
         args = realloc(args, sizeof(char*) * ++n_spaces);
         //check for successful reallocation
-
         if (args == NULL) {
             return NULL;
         }
