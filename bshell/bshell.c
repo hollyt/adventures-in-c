5a2467910374e0dedc33d76d3acdf9f06135048a
Holly Tancredi <tancredi.holly@gmail.com>
2016-01-06 13:25:15 -0500

    switch to getline

--- bshell/bshell.c
+++ bshell/bshell.c
@@ -3,6 +3,10 @@
 #include <string.h>
 
 void commands(void);
+char *parse_args(char *line);
+
+// starting small for now
+int MAX = 100;
 
 int main(int argc, char **argv) {
 
@@ -17,22 +21,35 @@ int main(int argc, char **argv) {
 }
 
 void commands() {
-    // starting out small for now
-    int MAX = 100;
-    char line[MAX+1];
+    char *line = NULL;
+    size_t len = 0;
     char **args;
     char *status;
 
     do {
         // kirby prompt
         printf("(>**)> ");
-        fgets(line, MAX, stdin);
+        getline(&line, &len, stdin);
+        free(line); 
         /*
-        args = parse_args(line);
+        args = parse_arg(line);
         status = execute(args);
 
-        free(line);
         free(args);
         */
     } while(status);
+}
+
+char *parse_args(char *line) {
+    int i = 0;
+    const char delims[2] = " ";
+    // for testing purposes ...
+    int SIZE = 10;
+    char *args[SIZE+1];
+
+    // strtok
+
+    for (i = 0; i < MAX; i++) {
+        
+    }   
 }
